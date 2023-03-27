5b1237af1ff0252ca6b6580314d7c7b1cd3bf505
wiktorjs <wiktorj.sienkiewicz@gmail.com>
2023-03-27 10:27:06 +0200

    Finish media queries, implement dynamic navigation

--- src/js/views/modalView.js
+++ src/js/views/modalView.js
@@ -3,29 +3,23 @@ import { OVERLAY_TRANSITION_SEC } from "../config";
 import { wait } from "../helpers";
 
 class ModalView {
-  _btnOpen = document.querySelectorAll(".btn-open-modal");
-  _btnClose = document.getElementById("btn-close-modal");
+  #btnOpen = document.querySelectorAll(".btn-open-modal");
+  #btnClose = document.getElementById("btn-close-modal");
 
-  _overlay = document.querySelector(".overlay");
-  _modalParent = document.querySelector(".modal-parent");
+  #overlay = document.querySelector(".overlay");
+  #modalParent = document.querySelector(".modal-parent");
 
-  constructor() {
-    this._overlay.style.transition = `opacity ${OVERLAY_TRANSITION_SEC}s ease`;
-    this._openModalListener();
-    this._hideModalListener();
-  }
-
-  async _hideModal() {
-    this._overlay.style.opacity = 0;
+  async #hideModal() {
+    this.#overlay.style.opacity = 0;
 
     // | Implemented for smooth transition - to make overlay and it's children disappear at the same time
     await wait(OVERLAY_TRANSITION_SEC);
 
-    this._overlay.classList.toggle("hidden");
+    this.#overlay.classList.toggle("hidden");
   }
 
-  _openModalListener() {
-    this._btnOpen.forEach((btn) =>
+  #openModalListener() {
+    this.#btnOpen.forEach((btn) =>
       btn.addEventListener("click", (e) => {
         // | Check if there was a model rendered eariler - if so, remove it
         const modalContent = document.querySelector(".modal__content");
@@ -37,32 +31,38 @@ class ModalView {
           modalType === "services" ? servicesMarkup : bookMarkup;
         //
 
-        modalType === "book" && this._modalParent.classList.add("modal-parent__book");
-        modalType === "services" && this._modalParent.classList.remove("modal-parent__book");
+        modalType === "book" && this.#modalParent.classList.add("modal-parent__book");
+        modalType === "services" && this.#modalParent.classList.remove("modal-parent__book");
   
         // | Insert desired modal
-        this._modalParent.insertAdjacentHTML("beforeend", modalMarkup);
+        this.#modalParent.insertAdjacentHTML("beforeend", modalMarkup);
 
         // | Show overlay
-        this._overlay.classList.toggle("hidden");
-        this._overlay.style.opacity = 1;
+        this.#overlay.classList.toggle("hidden");
+        this.#overlay.style.opacity = 1;
       })
     );
   }
 
-  _hideModalListener() {
-    this._btnClose.addEventListener("click", this._hideModal.bind(this));
+  #hideModalListener() {
+    this.#btnClose.addEventListener("click", this.#hideModal.bind(this));
 
     document.addEventListener("keydown", (e) => {
-      if (e.key === "Escape" && !this._overlay.classList.contains("hidden"))
-        this._hideModal();
+      if (e.key === "Escape" && !this.#overlay.classList.contains("hidden"))
+        this.#hideModal();
       return;
     });
 
-    this._overlay.addEventListener("click", (e) => {
-      if (e.target !== this._overlay) return;
-      this._hideModal();
+    this.#overlay.addEventListener("click", (e) => {
+      if (e.target !== this.#overlay) return;
+      this.#hideModal();
     });
+  }
+
+  init() {
+    this.#overlay.style.transition = `opacity ${OVERLAY_TRANSITION_SEC}s ease`;
+    this.#openModalListener();
+    this.#hideModalListener();
   }
 }
 
